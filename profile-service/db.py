4ec877b4d832b67cf7fa7fd4b238a29eb8da5e31
lukebainbridge1994 <lukebainbridge@live.com>
2026-01-16 13:08:15 +0000

    Finalise ProfileService with Docker deployment and swagger documentation

--- profile-service/db.py
+++ profile-service/db.py
@@ -6,9 +6,11 @@ load_dotenv()
 
 def get_connection():
     return pyodbc.connect(
-        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"DRIVER={{ODBC Driver 18 for SQL Server}};"
         f"SERVER={os.getenv('DB_SERVER')};"
         f"DATABASE={os.getenv('DB_NAME')};"
         f"UID={os.getenv('DB_USER')};"
-        f"PWD={os.getenv('DB_PASSWORD')}"
+        f"PWD={os.getenv('DB_PASSWORD')};"
+        f"Encrypt=yes;"
+        f"TrustServerCertificate=yes;"
     )
